85ad5cb058623bd10d044e0c6f6b900f87117e39
Jeganathan Swaminathan <mail2jegan@gmail.com>
2025-02-25 17:52:34 +0530

    Added Day2 lab exercises

---
+++ Day2/golang/hello.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+//Entry point function - default function that would be invoked first
+//This function must be defined under package main only
+func main() {
+  fmt.Println( "Hello Golang !" )
+}
