f9c37d4918a5fee1e151bd7457dbf0ad85c00ed2
Jeganathan Swaminathan <mail2jegan@gmail.com>
2025-02-27 11:26:45 +0530

    Added Day4 lab exercises

---
+++ Day4/terraform/provision-docker-container/main.tf
@@ -0,0 +1,29 @@
+resource "docker_image" "ubuntu_image" {
+  name = "ubuntu:latest"
+}
+
+resource "docker_container" "ubuntu_container1" {
+  name  = "ubuntu_container1"
+  hostname = "ubuntu_container1"
+  image = docker_image.ubuntu_image.name
+
+  must_run = true
+  command = [
+     "tail",
+     "-f",
+     "/dev/null"
+  ]
+}
+
+resource "docker_container" "ubuntu_container2" {
+  name  = "ubuntu_container2"
+  hostname = "ubuntu_container2"
+  image = docker_image.ubuntu_image.name
+
+  must_run = true
+  command = [
+     "tail",
+     "-f",
+     "/dev/null"
+  ]
+}
