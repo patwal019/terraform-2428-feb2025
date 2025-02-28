be48dc588637f41e265e557261d8b4bb2871459d
Jeganathan Swaminathan <mail2jegan@gmail.com>
2025-02-28 15:28:57 +0530

    Included local provider

--- Day5/DevOpsCICDPipeline/invoking-ansible-playbook-from-terraform/providers.tf
+++ Day5/DevOpsCICDPipeline/invoking-ansible-playbook-from-terraform/providers.tf
@@ -4,9 +4,17 @@ terraform {
       source = "tektutor/docker"
       version = "2.0.0"
     }
+    local = {
+      source = "hashicorp/local"
+      version = "2.5.2"
+    }
   }
 }
 
 provider "docker" {
+  # Configuration options
+}
+
+provider "local" {
   # Configuration options
 }
